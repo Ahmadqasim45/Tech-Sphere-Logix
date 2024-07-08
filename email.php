2c6a62cf84070789b7c238feacebe0aef92ad06b
Ahmad-bhattii <ahmedqasim0340@gmail.com>
2024-07-08 14:01:09 +0500

    ahmad

--- email.php
+++ email.php
@@ -5,16 +5,16 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
-require  'phpmailer\vendor\phpmailer\phpmailer\src\Exception.php';
-require  'phpmailer\vendor\phpmailer\phpmailer\src\PHPMailer.php';
-require  'phpmailer\vendor\phpmailer\phpmailer\src\SMTP.php';
+require 'phpmailer\vendor\phpmailer\phpmailer\src\Exception.php';
+require 'phpmailer\vendor\phpmailer\phpmailer\src\PHPMailer.php';
+require 'phpmailer\vendor\phpmailer\phpmailer\src\SMTP.php';
 
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
     $email = $_POST['email'];
+  
 
     // Create an instance; passing `true` enables exceptions
     $mail = new PHPMailer(true);
@@ -26,25 +26,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
         $mail->Host = 'smtp.gmail.com';                             // Set the SMTP server to send through
         $mail->SMTPAuth = true;                                     // Enable SMTP authentication
         $mail->Username = 'techspherelogix@gmail.com';              // SMTP username
-        $mail->Password = 'ybqjecuckfcaympd
-';                       // SMTP password
+        $mail->Password = 'ybqjecuckfcaympd';                       // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable implicit TLS encryption
         $mail->Port = 587;                                          // TCP port to connect to
 
         // Recipients
         $mail->setFrom($email, $name); // Sender email
-        $mail->addAddress('techspherelogix@gmail.com', 'Tech Sphere Logix'); // Recipient email
+        $mail->addAddress('techspherelogix.com', 'Tech Sphere Logix'); // Recipient email
 
         // Content
         $mail->isHTML(true);                                        // Set email format to HTML
         $mail->Subject = $subject;
-        $mail->Body = $message . '</br>';
+        $mail->Body    = $message . '</br>';
 
         $mail->send();
         echo 'Message has been sent';
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
-} else {
-    echo "Invalid request method.";
 }
